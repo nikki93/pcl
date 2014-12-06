9a173373b6dba5e39e9dcd57dbd1ce7e929a176e
nikki93 <s.nikhilesh@gmail.com>
2014-12-06 06:07:58 -0500

    finish cd database example, and also write 'update' as a macro

--- cd/cd.lisp
+++ cd/cd.lisp
@@ -4,11 +4,13 @@
   (list :title title :artist artist :rating rating :ripped ripped))
 
 
+
 ;; db
 
 (defvar *db* nil)
 
 (defun add-record (cd) (push cd *db*))
+
 
 (defun dump-db ()
   (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
@@ -29,9 +31,50 @@
   (loop (add-record (prompt-for-cd))
      (if (not (y-or-n-p "Anothers? [y/n]:")) (return))))
 
-(defun save-db ()
+
+(defun save-db (filename)
   (with-open-file (out filename
                        :direction :output
                        :if-exists :supersede)
     (with-standard-io-syntax
       (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+
+(defun make-comparison-expr (field value)
+  `(equal (getf cd ,field) ,value))
+
+(defun make-comparisons-list (fields)
+  (loop while fields
+       collecting (make-comparison-expr (pop fields) (pop fields))))
+
+(defmacro where (&rest clauses)
+  `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
+
+
+(defun make-update-expr (field value)
+  `(setf (getf row ,field) ,value))
+
+(defun make-updates-list (fields)
+  (loop while fields
+       collecting (make-update-expr (pop fields) (pop fields))))
+
+(defmacro update (selector-fn &rest updated-values)
+  `(setf *db*
+         (mapcar
+          #'(lambda (row)
+              (when (funcall ,selector-fn row)
+                ,@(make-updates-list updated-values))
+              row) *db*)))
+
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
