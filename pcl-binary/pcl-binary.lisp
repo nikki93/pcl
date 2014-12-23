8f25b02d11021800431cbb0e63dc64917f70fc53
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-12-23 18:17:02 +0300

    add define-tagged-binary-class, define-binary-type

--- pcl-binary/pcl-binary.lisp
+++ pcl-binary/pcl-binary.lisp
@@ -52,7 +52,37 @@
 
 
 ;;;
-;;; define-binary-class
+;;; define-binary-type
+;;;
+
+(defmacro define-binary-type (name (&rest args) &body spec)
+  (ecase (length spec)
+    (1
+     (with-gensyms (type stream val)
+       (let* ((spec (first spec))
+              (lspec (if (listp spec) spec (list spec))))
+        (destructuring-bind (parent &rest parent-args) lspec
+          `(progn
+             (defmethod read-value ((,type (eql ',name)) ,stream &key ,@args)
+               (read-value ',parent ,stream ,@parent-args))
+             (defmethod write-value ((,type (eql ',name)) ,stream ,val &key ,@args)
+               (read-value ',parent ,stream ,val ,@parent-args)))))))
+    (2
+     (with-gensyms (type)
+       `(progn
+          ,(destructuring-bind
+            ((in) &body body) (rest (assoc :reader spec))
+            `(defmethod read-value ((,type (eql ',name)) ,in &key ,@args)
+               ,@body))
+          ,(destructuring-bind
+            ((out val) &body body) (rest (assoc :writer spec))
+            `(defmethod write-value ((,type (eql ',name)) ,out ,val &key ,@args)
+               ,@body)))))))
+
+
+
+;;;
+;;; define-binary-class, define-tagged-binary-class
 ;;;
 
 ;;; macro helpers
@@ -77,6 +107,17 @@
          (args (if (listp second) (rest second))))
     `(write-value ',type ,stream ,name ,@args)))
 
+(defun slot->binding (spec stream)
+  (let* ((name (first spec)) 
+         (second (second spec))
+         (type (if (listp second) (first second) second))
+         (args (if (listp second) (rest second))))
+    `(,name (read-value ',type ,stream ,@args))))
+
+(defun slot->initarg (spec)
+  (let ((name (first spec)))
+    `(,(as-keyword name) ,name)))
+
 
 ;;; slot metadata
 
@@ -95,9 +136,10 @@
   (nconc (mapcan #'all-slots superclasses) (mapcar #'first slots)))
 
 
-;;; main macro
+;;; main macros
 
-(defmacro define-binary-class (class (&rest superclasses) slots)
+(defmacro define-generic-binary-class (class (&rest superclasses) slots
+                                       &body read-method)
   (with-gensyms (instance-var stream-var)
     `(progn
        ;; update metadata
@@ -109,15 +151,36 @@
        (defclass ,class ,superclasses
          ,(mapcar #'slot->defclass-slot slots))
 
-       ;; read/write
+       ;; read method
+       ,@read-method
+
+       ;; write method
+       (defmethod write-object progn ((,instance-var ,class) ,stream-var)
+         (with-slots ,(new-class-all-slots slots superclasses) ,instance-var
+           ,@(mapcar #'(lambda (slot) (slot->write-value slot stream-var))
+                     slots))))))
+
+(defmacro define-binary-class (class (&rest superclasses) slots)
+  (with-gensyms (instance-var stream-var)
+    `(define-generic-binary-class ,class ,superclasses ,slots
        (defmethod read-object progn ((,instance-var ,class) ,stream-var)
          (with-slots ,(new-class-all-slots slots superclasses) ,instance-var
            ,@(mapcar #'(lambda (slot) (slot->read-value slot stream-var))
-                     slots)))
-       (defmethod write-object progn ((,instance-var ,class) ,stream-var)
-         (with-slots ,(new-class-all-slots slots superclasses) ,instance-var
-           ,@(mapcar #'(lambda (slot) (slot->read-value slot stream-var))
                      slots))))))
+
+(defmacro define-tagged-binary-class (class (&rest superclasses)
+                                               slots &rest options)
+  (with-gensyms (instance-var stream-var type-var)
+    `(define-generic-binary-class ,class ,superclasses ,slots
+       (defmethod read-value ((,type-var (eql ',class)) ,stream-var &key)
+         (let* ,(mapcar #'(lambda (slot) (slot->binding slot stream-var))
+                        slots)
+           (let ((,instance-var
+                  (make-instance ,@(or (cdr (assoc :dispatch options))
+                                       (error "Must supply :displatch form."))
+                                 ,@(mapcan #'slot->initarg slots))))
+             (read-object ,instance-var ,stream-var)
+             ,instance-var))))))
 
 
 
@@ -132,3 +195,17 @@
    (flags u1)
    (size id3-tag-size)
    (frames (id3-frames :tag-size size))))
+
+(define-binary-type iso-8859-1-string (length)
+  (:reader (in)
+    (let ((string (make-string length)))
+      (dotimes (i length)
+        (setf (char string i) (code-char (read-byte in))))
+      string))
+  (:writer (out string)
+    (dotimes (i length)
+      (write-byte (char-code (char string i)) out))))
+
+(define-binary-type u1 () (unsigned-integer :bytes 1))
+
+
